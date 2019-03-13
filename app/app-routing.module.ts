20c729d116c3160c6c69a037dff3f9f05b39bff3
Justin Navarro <info@justinnavarro.net>
2019-03-13 01:25:11 -0400

    updates to app routing and modules

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import {ShowsComponent} from '~/shows/shows.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/shows', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'shows', component: ShowsComponent },
 ];
 
 @NgModule({
--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ActionBarComponent } from './action-bar/action-bar.component';
+import { ShowsComponent } from './shows/shows.component';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from 'nativescript-angular/forms';
@@ -15,6 +17,8 @@ import { HomeComponent } from './home/home.component';
   declarations: [
       AppComponent,
       HomeComponent,
+      ActionBarComponent,
+      ShowsComponent,
   ],
   imports: [
       NativeScriptModule,
