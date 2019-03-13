813a6d2bcce51cfd3b894d708b239f7334714eba
Justin Navarro <info@justinnavarro.net>
2019-03-13 01:34:23 -0400

    rename css to scss

--- app/shows/shows.component.ts
+++ app/shows/shows.component.ts
@@ -3,14 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-shows',
   templateUrl: './shows.component.html',
-  styleUrls: ['./shows.component.css'],
+  styleUrls: ['./shows.component.scss'],
   moduleId: module.id,
 })
 export class ShowsComponent implements OnInit {
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
