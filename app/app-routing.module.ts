b3aa1b323013c7e5cd4c0a0c3582a585a24865f5
Justin Navarro <info@justinnavarro.net>
2019-03-20 15:25:04 -0400

    moved components to specific directories

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import {ShowsComponent} from '~/shows/shows.component';
+import { HomeComponent } from './pages/home/home.component';
+import {ShowsComponent} from '~/pages/shows/shows.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/shows', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'shows', component: ShowsComponent },
+    { path: '', redirectTo: '/shows', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'shows', component: ShowsComponent },
 ];
 
 @NgModule({
-  imports: [NativeScriptRouterModule.forRoot(routes)],
-  exports: [NativeScriptRouterModule]
+    imports: [NativeScriptRouterModule.forRoot(routes)],
+    exports: [NativeScriptRouterModule]
 })
 export class AppRoutingModule { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,9 @@ import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ActionBarComponent } from './action-bar/action-bar.component';
-import { ShowsComponent } from './shows/shows.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ActionBarComponent } from './components/action-bar/action-bar.component';
+import { ShowsComponent } from './pages/shows/shows.component';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from 'nativescript-angular/forms';
