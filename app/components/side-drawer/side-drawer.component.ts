ef901fea4cfdebcc83c94146be4e719f33aa9615
Justin Navarro <info@justinnavarro.net>
2019-03-21 01:46:51 -0400

    build out the side drawer links

--- app/components/side-drawer/side-drawer.component.ts
+++ app/components/side-drawer/side-drawer.component.ts
@@ -7,11 +7,11 @@ import {Component, OnInit} from '@angular/core';
     moduleId: module.id,
 })
 export class SideDrawerComponent implements OnInit {
-    pages: Array<{ icon: string; text: string; link: string; }> = [
-        { icon: '', text: 'Shows', link: '' },
-        { icon: '', text: 'Lists', link: '' },
-        { icon: '', text: 'Movies', link: '' },
-        { icon: '', text: 'Statistics', link: '' },
+    pages: Array<{ icon: string; text: string; link: string[]; }> = [
+        { icon: String.fromCharCode(0xf26c), text: 'Shows', link: ['/shows'] },
+        { icon: String.fromCharCode(0xf03a), text: 'Lists', link: ['/'] },
+        { icon: String.fromCharCode(0xf008), text: 'Movies', link: ['/'] },
+        { icon: String.fromCharCode(0xf080), text: 'Statistics', link: ['/'] },
     ];
 
     ngOnInit() {}
